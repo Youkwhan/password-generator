ad9117a209455c30a685fbb349a7a805c40b2d8b
Youkwhan <Youkwhan@gmail.com>
2023-03-26 00:00:44 -0400

    password logic

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+/*
+two 15 characters long password
+Interactions:
+   - Generate password button
+   - Reset button
+   - password1 p
+   - password2 p 
 
-// two 15 characters long password
-// set password length
-// copy on click
-// toggle numbers or symbol
 
+Extra:
+   set password length
+   copy on click
+   toggle numbers or symbol
 
+*/
 
+let password1El = document.getElementsByClassName("password1")[0];
+let password2El = document.getElementsByClassName("password2")[0];
 
+//void
+function setPassword(){
+   password1El.textContent = getPassword();
+   password2El.textContent = getPassword();
+}
 
+// return 15letter[str]
+function getPassword() {
+   let tempPassword = [];
+   for (let i = 0; i < 15; i++){
+      tempPassword.push(getRandomChar())
+   }
+   return tempPassword.join("");
+}
+
+// return char[str]
+function getRandomChar() {
+   let randomIndex = Math.floor(Math.random() * characters.length);
+   return characters[randomIndex];
+}
+
+function resetPassword() {
+   password1El.textContent = ""
+   password2El.textContent = ""
+}
