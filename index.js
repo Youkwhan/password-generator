cf33d9138d6b738a442507d5a7eae721c3681826
Youkwhan <Youkwhan@gmail.com>
2023-03-26 16:28:46 -0400

    password length and error msg

--- index.js
+++ index.js
@@ -16,20 +16,32 @@ Extra:
    toggle numbers or symbol
 
 */
-
+let passwordLengthInput = document.getElementById("password-length");
 let password1El = document.getElementsByClassName("password1")[0];
 let password2El = document.getElementsByClassName("password2")[0];
 
 //void
 function setPassword(){
-   password1El.textContent = getPassword();
-   password2El.textContent = getPassword();
+   let passwordLength = parseInt(passwordLengthInput.value);
+   let errorDiv = document.getElementById("error");
+   
+   if (validateInput(passwordLength)){
+      password1El.textContent = getPassword(passwordLength);
+      password2El.textContent = getPassword(passwordLength);
+   } else {
+      // error message 
+      //  console.log("not valid length")
+      errorDiv.textContent = "Must be a number between 1 and 15";
+      setTimeout(() => {
+         errorDiv.textContent = "";
+      }, 2000);
+   }
 }
 
 // return 15letter[str]
-function getPassword() {
+function getPassword(passwordLength) {
    let tempPassword = [];
-   for (let i = 0; i < 15; i++){
+   for (let i = 0; i < passwordLength; i++){
       tempPassword.push(getRandomChar())
    }
    return tempPassword.join("");
@@ -41,7 +53,18 @@ function getRandomChar() {
    return characters[randomIndex];
 }
 
+// void
 function resetPassword() {
-   password1El.textContent = ""
-   password2El.textContent = ""
+   password1El.textContent = "";
+   password2El.textContent = "";
+   passwordLengthInput.textContent = "15";
+}
+
+
+// return bool, is input length in our range
+function validateInput(passwordLength) {
+   if (isNaN(passwordLength) || passwordLength < 1 || passwordLength > 15) {
+       return false;
+   }
+   return true;
 }
